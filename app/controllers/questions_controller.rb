d33763f4fe0e08da1288b90b2d159243f336fa7a
lakshit gupta <lakshitgupta218@gmail.com>
2024-03-19 19:59:42 +0530

    added result page with score

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,19 +1,9 @@
 class QuestionsController < ApplicationController
   def index
     @student = Student.find(params[:student_id]) if params[:student_id].present?
-
     @questions = Question.all.includes(:options)
   end
-  def submit_answers
-    @questions = Question.find(params[:question_ids])
-    total_marks = 0
-    @questions.each do |question|
-      correct_option = question.correct_option
-      selected_option = params[:answers][question.id.to_s].to_i
-      if selected_option == correct_option
-        total_marks += 1
-      end
-    end
-    render plain: "total marks #{total_marks}"
-  end
 end
+
+
+
--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,36 @@
 class ResultsController < ApplicationController
   def show
     @student_id = params[:student_id]
-    # Use @student_id as needed in the show action
+    @student = Student.find(@student_id)
+    
+    # Loop through the params to fetch the selected option names for each question
+    @selected_options = {}
+    params.each do |key, value|
+      if key.start_with?('question_')
+        question_id = key.split('_').last.to_i
+        @selected_options[question_id] = value
+      end
+    end
+
+    # Calculate the score
+    @score = calculate_score(@selected_options)
+  end
+
+  private
+  
+  def calculate_score(selected_options)
+    score = 0
+
+    # Loop through each question and compare the selected option with the correct option
+    (5..11).each do |question_number|
+      selected_option_name = selected_options[question_number]
+      correct_option_name = Option.find(question_number).send("option#{Option.find(question_number).correct_option}")
+
+      if selected_option_name == correct_option_name
+        score += 1
+      end
+    end
+
+    score
   end
 end
